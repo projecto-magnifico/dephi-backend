09c7fc14a6d503f6753b08d13d016984607fdfa8
Jonny Rathbone <jonnyrathbone@gmail.com>
2017-10-26 09:47:27 +0100

    corrected lambda process functions (for topic-to-thread)

--- lambda/utils/index.js
+++ lambda/utils/index.js
@@ -6,27 +6,38 @@ const getKeywordSets = (topicsKeywords) => {
     const keywordSets = topicsKeywords.map(topicKeywords => {
         const keywordsRelevanceArray = topicKeywords.map(topicKeyword => {
             let topicKeywordArray = getSets(topicKeyword.text);
+            // console.log(topicKeywordArray);
             const splitKeywordsWithRelevance = topicKeywordArray.map(topicKeywordWord => {
                 return {
                     text : topicKeywordWord.toLowerCase(),
                     relevance : topicKeywordWord === topicKeyword.text ? topicKeyword.relevance : topicKeyword.relevance / 2
                 };
             });
+            // console.log(splitKeywordsWithRelevance);
+            
             return splitKeywordsWithRelevance;
         });
-        return _.flatten(keywordsRelevanceArray);
+        // console.log('********')
+        let unique = _.uniq(_.flatten(keywordsRelevanceArray), false, extractKeywords);
+        // console.log(unique);
+        return unique;
     });
     return keywordSets;
 };
 
+function extractKeywords (object) {
+    return object.text;
+}
+
 const formulateInsertionSchema = ( topicKeywordSets, threadKeywordSets ) => {
     let insertionSchema = [];
+    let newThreadSchema = [];
     topicKeywordSets.forEach((topicKeywords,i) => {
-        // console.log(topicKeywordSets)
+        newThreadSchema.push(i);
         threadKeywordSets.forEach((threadKeywords) => {
             let topicKeywordsText = _.pluck(topicKeywords,'text');
             let threadKeywordsText = _.pluck(threadKeywords,'text');
-            let intersection = (_.intersection(topicKeywordsText,threadKeywordsText));
+            let intersection = _.uniq(_.intersection(topicKeywordsText,threadKeywordsText));
             let difference = _.uniq(_.difference(topicKeywordsText,threadKeywordsText));
             if (intersection.length > 3) {
                 let intersectedWordObjects = topicKeywordSets[i].filter(wordObject => {
@@ -40,10 +51,11 @@ const formulateInsertionSchema = ( topicKeywordSets, threadKeywordSets ) => {
                     boostKeywords : intersectedWordObjects,
                     newKeywords : differentWordObjects
                 });
+                newThreadSchema = _.without(newThreadSchema, i);
             }
         });
     });
-    return insertionSchema;
+    return {newThreadSchema, insertionSchema};
 };
 
 module.exports = { getKeywordSets, formulateInsertionSchema };
